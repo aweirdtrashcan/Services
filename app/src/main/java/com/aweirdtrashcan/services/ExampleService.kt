de52af309f2ef65d35a22938b8ef4f151b19fdb8
aweirdtrashcan <diegodrp01@gmail.com>
2022-07-17 09:04:29 -0300

    Final Commit

--- app/src/main/java/com/aweirdtrashcan/services/ExampleService.kt
+++ app/src/main/java/com/aweirdtrashcan/services/ExampleService.kt
@@ -1,9 +1,8 @@
 package com.aweirdtrashcan.services
 
-import android.app.IntentService
 import android.app.Service
 import android.content.Intent
-import android.os.IBinder
+import android.os.*
 import android.widget.Toast
 
 //
@@ -22,3 +21,48 @@ import android.widget.Toast
 //        }
 //    }
 //}
+
+class ExampleService: Service() {
+
+    private var serviceLooper: Looper? = null
+    private var serviceHandler: ServiceHandler? = null
+
+    private inner class ServiceHandler(looper: Looper): Handler(looper) {
+        override fun handleMessage(msg: Message) {
+            try {
+                Thread.sleep(5000)
+            } catch (e: InterruptedException) {
+                Thread.currentThread().interrupt()
+            }
+
+            stopSelf(msg.arg1)
+        }
+    }
+
+    override fun onCreate() {
+        HandlerThread("ServiceStartArguments", Process.THREAD_PRIORITY_BACKGROUND).apply {
+            start()
+
+            serviceLooper = looper
+            serviceHandler = ServiceHandler(looper)
+        }
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Toast.makeText(this, "service starting", Toast.LENGTH_LONG).show()
+
+        serviceHandler?.obtainMessage()?.also { msg ->
+            msg.arg1 = startId
+            serviceHandler?.sendMessage(msg)
+        }
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onDestroy() {
+        Toast.makeText(this, "service done", Toast.LENGTH_LONG).show()
+    }
+}
